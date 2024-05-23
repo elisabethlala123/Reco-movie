c21ada6e3bfaa7d4d047eb2ea62107efa65491f0
elisabethlala123 <elisabethlala123@gmail.com>
2024-05-23 13:23:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
